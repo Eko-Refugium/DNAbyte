f56dec82982ad1833f8efbf56f06c0955452f155
SchroederFabian <fabian.schroeder@ekorefugium.com>
2025-12-02 10:56:04 +0100

    base data class direct import

--- dnabyte/data_classes/__init__.py
+++ dnabyte/data_classes/__init__.py
@@ -2,11 +2,13 @@
 Data classes for DNAbyte - representations of data at different stages of encoding/decoding.
 """
 
+from .base import Data
 from .binarycode import BinaryCode
 from .nucleobasecode import NucleobaseCode
 from .insilicodna import InSilicoDNA
 
 __all__ = [
+    "Data",
     "BinaryCode",
     "NucleobaseCode", 
     "InSilicoDNA",
